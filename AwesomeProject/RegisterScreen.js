110daf8ce3fc704f7e98b6e85016b04d791c3d59
AzizAldosari <99676859+AzizAldosari@users.noreply.github.com>
2023-01-19 22:02:44 -0600

    aw

---
+++ AwesomeProject/RegisterScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View , Text , StyleSheet} from "react-native";
+export default class RegisterScreen extends React.Component {
+    render() {
+ return (
+  < View style  = {StyleSheet.container}>
+<Text>Register screen</Text>
+
+  </View>
+ );
+
+    }
+}
+const styles = StyleSheet.create({
+  container: {
+flex: 1,
+justifyContent: "center" ,
+alignItems: "center"
+
+  }
+});
